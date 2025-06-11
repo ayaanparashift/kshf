6c4c7bfcba64148bf319af7e144571595c2120de
ayaanparashift <ayaan.s@parashifttech.com>
2025-06-11 12:18:57 +0530

    timeline done

--- src/app/time/TimeLine.jsx
+++ src/app/time/TimeLine.jsx
@@ -284,38 +284,38 @@ const timelineData = [
     year: 1979,
     content:
       "Expanded the Bosch products’ distribution business into Pune city, Maharashtra by taking over the operations of 'Waterloo Motors'",
-    image: "/time/t2.jpg",
+    image: "/time/t1.jpg",
   },
   {
-    id: 4,
+    id: 3,
     year: 1981,
     content:
       "Founded 'KSH International’ - Mumbai plant, with 360 MT annual commercial production of Paper Insulated Copper Conductors (PICC)and forayed into manufacturing",
     image: "/time/t4.jpg",
   },
   {
-    id: 5,
+    id: 4,
     year: 1989,
     content:
       "Established ‘Kushal Motors’ in Pune city, Maharashtra to cater the original equipment customers of Bosch.",
     image: "/time/t3.jpg",
   },
   {
-    id: 6,
+    id: 5,
     year: 1991,
     content:
       "Established Kushal Electricals to distribute and service the Electrical Power Tools product line of Bosch.",
     image: "/time/t5.jpg",
   },
   {
-    id: 7,
+    id: 6,
     year: 1999,
     content:
       "Established 'KSH Logistics asa Third-party Logistics (3PL) company to service the captive requirements of Bosch in the Western Region of India and forayed into Logistics.",
     image: "/time/t6.jpg",
   },
   {
-    id: 8,
+    id: 7,
     year: 2002,
     content:
       "Established 'KSH International’ - Pune plant with additional annual capacity of 2400 MT and launched a new product line —- Enameled Copper Conductors.",
@@ -323,110 +323,75 @@ const timelineData = [
   },
 
   {
-    id: 9,
+    id: 8,
     year: 2008,
     content:
       "Established *KSH Distriparks' with the setting up of an Inland container Depot in Talegaon, Pune. The Dry Port facility was set up with a capacity of 120,000 TEUs per annum.",
     image: "/time/t8.jpg",
   },
   {
-    id: 10,
+    id: 9,
     year: 2012,
     content:
       "Investment received from Pacific Century Group of HK/Singapore to expand KSH Logistics, KSH Distriparks and set up ‘KSH INFRA’ to expand into the INFRA space",
     image: "/time/t9.jpg",
   },
   {
-    id: 11,
+    id: 10,
     year: 2016,
     content:
       "KSH International became the market leader with annual manufacturing ‘capacity of 20,000 MT of ‘which 12,000 MT comprises of Continuously Transposed Conductors (CTC)",
     image: "/time/t10.jpg",
   },
   {
-    id: 12,
+    id: 11,
     year: 2018,
     content:
       "Established two projects under 'KSH INFRA! admeasuring 1 Million sqft, and successfully exited the projects to Morgan Stanley Investment Fund, USA",
     image: "/time/t11.jpg",
   },
   {
-    id: 13,
+    id: 12,
     year: 2018,
     content:
       "KSH Group celebrated its Golden Jubilee year and expressed their gratitude to Bosch for having played a leading role in its growth story.",
     image: "/time/t12.jpg",
   },
   {
-    id: 14,
+    id: 13,
     year: 2019,
     content:
       "KSH INFRA successfully built, leased and exited its project admeasuring 1.2 Million sqft to Mapletree Investments, Singapore.",
     image: "/time/t13.jpg",
   },
   {
-    id: 15,
+    id: 14,
     year: 2021,
     content:
       "KSH INFRA partnered with Indospace Capital to invest INR 1000 crores in a joint venture (JV) and commenced construction on its first project under the JV of 1.6 Million sqft with an aim to develop 10 Million sqft across key markets in India.",
     image: "/time/t14.jpg",
   },
   {
-    id: 16,
+    id: 15,
     year: 2023,
     content:
       "KSH International surpasses a turnover of 1000 CR taking the KSH Group turnover well above 1500 CR.",
     image: "/time/t15.jpg",
   },
   {
-    id: 17,
+    id: 16,
     year: 2024,
     content:
       "KSH INFRA progresses in South India with land acquisition and planning for Hosur Park I.",
     image: "/time/t18.jpg",
   },
-  // {
-  //   id: 18,
-  //   year: 2018,
-  //   content:
-  //     "Significant expansion planned in Southern India, an initiative that underscores KSH INFRA's commitment to broadening our footprint in strategically important regions.",
-  //   image: "/time/t19.jpg",
-  // },
-  // {
-  //   id: 19,
-  //   year: 2019,
-  //   content:
-  //     "Started construction of the third Industrial & Logistics Park in Chakan, Pune with a planned development of 1.2 million square feet for Warehousing/Industrial Infrastructure with Mapletree Investments of Singapore.",
-  //   image: "/time/t20.jpg",
-  // },
-  // {
-  //   id: 20,
-  //   year: 2021,
-  //   content:
-  //     "IndoSpace has agreed to invest INR 1000 crores in a joint venture (JV) with KSH INFRA to develop a portfolio of 10 million square feet of world-class warehousing and industrial infrastructure across the country.",
-  //   image: "/time/t21.jpg",
-  // },
-  // {
-  //   id: 21,
-  //   year: 2023,
-  //   content:
-  //     "KSH International surpasses a turnover of 1000 CR, taking the KSH Group well above 1500 CR",
-  //   image: "/time/t22.jpg",
-  // },
-  // {
-  //   id: 22,
-  //   year: 2024,
-  //   content:
-  //     "KSH INFRA progresses in South India with land acquisition and planning for Hosur Park I",
-  //   image: "/time/t23.jpg",
-  // },
-  // {
-  //   id: 23,
-  //   year: 2025,
-  //   content:
-  //     "Significant expansion with acquisition of 60 acre land in Chennai and construction commencement of Hosur Park I",
-  //   image: "/time/t24.jpg",
-  // },
+  {
+    id: 17,
+    year: 2025,
+    content:
+      "Significant expansion planned in Southern India, an initiative that underscores KSH INFRA commitment to broadening our footprint n strategically important regions.",
+    image: "/time/t24.jpg",
+  },
 ];
 //
 //
