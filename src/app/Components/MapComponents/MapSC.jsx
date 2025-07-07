b91f7ece85366bd97988013add675e2b01debb37
ayaanparashift <ayaan.s@parashifttech.com>
2025-07-07 10:37:59 +0530

    map slider content reveal and exit animation implemented

--- src/app/Components/MapComponents/MapSC.jsx
+++ src/app/Components/MapComponents/MapSC.jsx
@@ -1,15 +1,104 @@
+// "use client";
+// import Link from "next/link";
+// import React from "react";
+// import { motion } from "framer-motion";
+// const MapSC = ({ parkimg, title, city, tarea, tbuild, parkLink }) => {
+//   return (
+//     <>
+//       <motion.div
+//         initial={{ opacity: 0 }}
+//         whileInView={{ opacity: 1 }}
+//         transition={{ duration: 0.3, ease: [0.7, 0, 0.4, 1] }}
+//         className="relative pb-[65px]"
+//       >
+//         <div className="h-[272px] flex items-center justify-between px-10 bg-white gap-10 w-full">
+//           <div className="flex-[0.5]">
+//             <img src={parkimg} className="object-cover" alt="first infra" />
+//           </div>
+//           <div className="flex flex-col gap-10 flex-1 w-full">
+//             <h2 className="pb-[15px] text-5xl border-b-[10px] border-[#F7E327] text-[#092241] font-bold overflow-hidden text-ellipsis max-w-fit">
+//               {title}
+//             </h2>
+//             <div className="flex flex-col gap-[5px] w-full">
+//               <div className="flex w-full min-w-0">
+//                 <p className="w-[70%] flex-1 text-[#092241]">CITY</p>
+//                 <p className="w-[30%] flex-1 text-[#092241]">{city}</p>
+//               </div>
+//               <div className="flex w-full min-w-0">
+//                 <p className="w-[70%] flex-1 text-[#092241]">TOTAL AREA</p>
+//                 <p className="w-[30%] flex-1 text-[#092241]">{tarea}</p>
+//               </div>
+//               <div className="flex w-full min-w-0">
+//                 <p className="w-[70%] flex-1 text-[#092241]">TOTAL BUILDINGS</p>
+//                 <p className="w-[30%] flex-1 text-[#092241]">{tbuild}</p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="pl-[6px] flex w-full justify-start h-fit">
+//           <Link
+//             href={parkLink}
+//             className="flex mt-5 group transition-transform duration-[400ms] items-center justify-start gap-3"
+//           >
+//             <img
+//               src="/buttonarrows/redarico.svg"
+//               width={0}
+//               height={0}
+//               className="h-10 w-10 rotate-45 group-hover:rotate-0 group-hover:scale-[120%] transition-transform duration-300 cursor-pointer"
+//               alt="Red Arrow Icon"
+//             />
+//             <p className="text-[#ffffff] text-[16px] fsans-600">Explore More</p>
+//           </Link>
+//         </div>
+//       </motion.div>
+//     </>
+//   );
+// };
+
+// export default MapSC;
+
+"use client";
 import Link from "next/link";
 import React from "react";
+import { motion } from "framer-motion";
 
-const MapSC = ({ parkimg, title, city, tarea, tbuild, parkLink }) => {
+const MapSC = ({
+  parkimg,
+  title,
+  city,
+  tarea,
+  tbuild,
+  parkLink,
+  isVisible = false,
+}) => {
   return (
     <>
       <div className="relative pb-[65px]">
         <div className="h-[272px] flex items-center justify-between px-10 bg-white gap-10 w-full">
-          <div className="flex-[0.5]">
-            <img src={parkimg} className="object-cover" alt="first infra" />
-          </div>
-          <div className="flex flex-col gap-10 flex-1 w-full">
+          <motion.div
+            key={title}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{
+              opacity: 0,
+              transition: { duration: 1, delay: 0, ease: [0.7, 0, 0.4, 1] },
+            }}
+            transition={{ duration: 0.5, ease: [0.7, 0, 0.4, 1], delay: 1 }}
+            className="flex-[0.5]"
+          >
+            <img src={parkimg} className="object-cover" alt="infra" />
+          </motion.div>
+          <motion.div
+            key={city}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{
+              opacity: 0,
+              transition: { duration: 0.5, delay: 0, ease: [0.7, 0, 0.4, 1] },
+            }}
+            transition={{ duration: 0.5, ease: [0.7, 0, 0.4, 1], delay: 1 }}
+            className="flex flex-col gap-10 flex-1 w-full"
+          >
             <h2 className="pb-[15px] text-5xl border-b-[10px] border-[#F7E327] text-[#092241] font-bold overflow-hidden text-ellipsis max-w-fit">
               {title}
             </h2>
@@ -27,7 +116,7 @@ const MapSC = ({ parkimg, title, city, tarea, tbuild, parkLink }) => {
                 <p className="w-[30%] flex-1 text-[#092241]">{tbuild}</p>
               </div>
             </div>
-          </div>
+          </motion.div>
         </div>
         <div className="pl-[6px] flex w-full justify-start h-fit">
           <Link
@@ -36,8 +125,6 @@ const MapSC = ({ parkimg, title, city, tarea, tbuild, parkLink }) => {
           >
             <img
               src="/buttonarrows/redarico.svg"
-              width={0}
-              height={0}
               className="h-10 w-10 rotate-45 group-hover:rotate-0 group-hover:scale-[120%] transition-transform duration-300 cursor-pointer"
               alt="Red Arrow Icon"
             />
--- src/app/Components/MapComponents/MapSlideDesk.jsx
+++ src/app/Components/MapComponents/MapSlideDesk.jsx
@@ -227,7 +227,134 @@
 //
 //
 //
+// "use client";
+// import { motion, AnimatePresence } from "framer-motion";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation } from "swiper/modules";
+// import MapSC from "./MapSC";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+// import { IoMdClose } from "react-icons/io";
+
+// const MapSlideDesk = ({
+//   activeHotspot,
+//   setActiveHotspot,
+//   showNavigation,
+//   selectedMaps,
+//   hotspots,
+// }) => {
+//   // Find the active hotspot data
+//   const activeHotspotData = hotspots.find((h) => h.id === activeHotspot);
+//   console.log(activeHotspotData);
+//   return (
+//     <div className="hotspot_col_two xl:flex hidden">
+//       <AnimatePresence>
+//         {activeHotspot && activeHotspotData && (
+//           <motion.div
+//             layout
+//             initial={{ width: "0px", height: "180px" }}
+//             animate={{
+//               width: "710px",
+//               height: "385px",
+//               transformOrigin: "0% 50%",
+//             }}
+//             exit={{
+//               width: "0px",
+//               height: "180px",
+//               transition: { duration: 1, ease: [0.7, 0, 0.4, 1], delay: 0.2 },
+//             }}
+//             transition={{ duration: 1, ease: [0.7, 0, 0.4, 1], delay: 0.2 }}
+//             className="overflow-hidden origin-top flex !self-start flex-col items-center relative"
+//             key={activeHotspotData.id}
+//           >
+//             <div className="w-full flex justify-end p-3">
+//               <button
+//                 onClick={() => setActiveHotspot(null)}
+//                 className="w-8 h-8 bg-white text-white flex items-center justify-center rounded-full text-lg font-bold absolute top-[32px] z-10 right-[9px]"
+//               >
+//                 <IoMdClose className="text-black text-2xl" />
+//               </button>
+//             </div>
+
+//             <div className="w-[710px] flex flex-col">
+//               <Swiper
+//                 modules={[Navigation]}
+//                 navigation={
+//                   showNavigation
+//                     ? { prevEl: ".prev-btn", nextEl: ".next-btn" }
+//                     : {}
+//                 }
+//                 slidesPerView={1.06}
+//                 spaceBetween={20}
+//                 breakpoints={{
+//                   1024: {
+//                     slidesPerView: 1,
+//                   },
+//                 }}
+//                 className="w-full h-full"
+//                 onInit={(swiper) => {
+//                   console.log("Swiper initialized", swiper.navigation);
+//                   console.log(
+//                     "Prev button:",
+//                     document.querySelector(".prev-btn")
+//                   );
+//                   console.log(
+//                     "Next button:",
+//                     document.querySelector(".next-btn")
+//                   );
+//                 }}
+//               >
+//                 {selectedMaps.map((mapd) => (
+//                   <SwiperSlide key={mapd.title + mapd.city}>
+//                     <MapSC
+//                       parkimg={mapd.parkimg}
+//                       title={mapd.title}
+//                       tarea={mapd.tarea}
+//                       tbuild={mapd.tbuild}
+//                       city={mapd.city}
+//                       parkLink={mapd.parkLink}
+//                     />
+//                   </SwiperSlide>
+//                 ))}
+//               </Swiper>
+//             </div>
+
+//             {showNavigation && (
+//               <div className="flex flex-row justify-end gap-[10px] mt-[0px] absolute bottom-[25px] z-10 w-fit right-0">
+//                 <button className="prev-btn arrowleft h-[41px] w-[41px] z-10 pointer-events-auto">
+//                   <img
+//                     className="h-[41px] w-[41px] bg-white rounded-full"
+//                     src="/buttonarrows/arrowleft.svg"
+//                     alt="ArrowLeft"
+//                   />
+//                 </button>
+//                 <button className="next-btn arrowright h-[41px] w-[41px] pointer-events-auto">
+//                   <img
+//                     className="h-[41px] w-[41px] bg-white rounded-full"
+//                     src="/buttonarrows/arrowright.svg"
+//                     alt="ArrowRight"
+//                   />
+//                 </button>
+//               </div>
+//             )}
+//           </motion.div>
+//         )}
+//       </AnimatePresence>
+//     </div>
+//   );
+// };
+
+// export default MapSlideDesk;
+
+//
+//
+//
+//
+//
+//
 "use client";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
@@ -244,9 +371,20 @@ const MapSlideDesk = ({
   selectedMaps,
   hotspots,
 }) => {
-  // Find the active hotspot data
   const activeHotspotData = hotspots.find((h) => h.id === activeHotspot);
-  console.log(activeHotspotData);
+
+  // local trigger for isVisible
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (activeHotspot) {
+      const timer = setTimeout(() => setIsVisible(true), 200);
+      return () => clearTimeout(timer);
+    } else {
+      setIsVisible(false);
+    }
+  }, [activeHotspot]);
+
   return (
     <div className="hotspot_col_two xl:flex hidden">
       <AnimatePresence>
@@ -268,15 +406,28 @@ const MapSlideDesk = ({
             className="overflow-hidden origin-top flex !self-start flex-col items-center relative"
             key={activeHotspotData.id}
           >
+            {/* CLOSE BUTTON */}
             <div className="w-full flex justify-end p-3">
-              <button
+              <motion.button
+                initial={{ opacity: 0 }}
+                animate={{ opacity: isVisible ? 1 : 0 }}
+                transition={{
+                  duration: 0.5,
+                  ease: [0.7, 0, 0.4, 1],
+                  delay: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 1, ease: [0.7, 0, 0.4, 1] },
+                }}
                 onClick={() => setActiveHotspot(null)}
                 className="w-8 h-8 bg-white text-white flex items-center justify-center rounded-full text-lg font-bold absolute top-[32px] z-10 right-[9px]"
               >
                 <IoMdClose className="text-black text-2xl" />
-              </button>
+              </motion.button>
             </div>
 
+            {/* SWIPER */}
             <div className="w-[710px] flex flex-col">
               <Swiper
                 modules={[Navigation]}
@@ -293,17 +444,6 @@ const MapSlideDesk = ({
                   },
                 }}
                 className="w-full h-full"
-                onInit={(swiper) => {
-                  console.log("Swiper initialized", swiper.navigation);
-                  console.log(
-                    "Prev button:",
-                    document.querySelector(".prev-btn")
-                  );
-                  console.log(
-                    "Next button:",
-                    document.querySelector(".next-btn")
-                  );
-                }}
               >
                 {selectedMaps.map((mapd) => (
                   <SwiperSlide key={mapd.title + mapd.city}>
@@ -314,14 +454,29 @@ const MapSlideDesk = ({
                       tbuild={mapd.tbuild}
                       city={mapd.city}
                       parkLink={mapd.parkLink}
+                      isVisible={isVisible}
                     />
                   </SwiperSlide>
                 ))}
               </Swiper>
             </div>
 
+            {/* SWIPER NAV BUTTONS */}
             {showNavigation && (
-              <div className="flex flex-row justify-end gap-[10px] mt-[0px] absolute bottom-[25px] z-10 w-fit right-0">
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: isVisible ? 1 : 0 }}
+                transition={{
+                  duration: 0.5,
+                  ease: [0.7, 0, 0.4, 1],
+                  delay: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 1, ease: [0.7, 0, 0.4, 1] },
+                }}
+                className="flex flex-row justify-end gap-[10px] mt-[0px] absolute bottom-[25px] z-10 w-fit right-0"
+              >
                 <button className="prev-btn arrowleft h-[41px] w-[41px] z-10 pointer-events-auto">
                   <img
                     className="h-[41px] w-[41px] bg-white rounded-full"
@@ -336,7 +491,7 @@ const MapSlideDesk = ({
                     alt="ArrowRight"
                   />
                 </button>
-              </div>
+              </motion.div>
             )}
           </motion.div>
         )}
